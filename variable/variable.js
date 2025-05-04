ba43f6e6f08af984edc8b87b3914103ab5cfabb1
Koushik Chowdhury <csakoushik@gmail.com>
2025-05-04 13:35:56 +0530

    variable in ts

---
+++ variable/variable.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = 'Koushik chowdhury';
+var age = 22.5;
+console.log("Hello, my name is ".concat(greetings, " and i am ").concat(age, " years old"));
+// number
+var userId = 12345;
+// boolean
+var isLoggedIn = true;
+// any
+var hero;
+function getHero() {
+    return 'Iron Man';
+}
+hero = getHero();
+hero = 'My Crush is -----';
+console.log(hero);
---
+++ variable/variable.ts
@@ -0,0 +1,28 @@
+let greetings= 'Koushik chowdhury'
+
+let age = 22.5
+
+console.log(`Hello, my name is ${greetings} and i am ${age} years old`)
+
+
+// number
+
+let userId: number = 12345
+
+// boolean
+
+let isLoggedIn: boolean = true
+
+// any
+
+let hero : string
+
+function getHero (){
+    return 'Iron Man'
+}
+hero = getHero()
+
+hero = 'My Crush is -----'
+
+console.log(hero)
+export { }
